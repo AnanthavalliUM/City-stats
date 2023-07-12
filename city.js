7a3a8ceb8bdaf1c5442f7b540a77ebca1c29ba60
Ananthavalli <100471735+AnanthavalliUM@users.noreply.github.com>
2023-07-12 12:21:04 +0530

    Add files via upload

---
+++ city.js
@@ -0,0 +1,37 @@
+const buttonelement = document.querySelector('button');
+let resultdiv = document.createElement('div');
+resultdiv.id = 'result'
+document.getElementById('wrapper').appendChild(resultdiv);
+buttonelement.addEventListener('click' , dislplaystats)
+function dislplaystats(){
+    const inputelement = document.getElementById('input')
+    const city = inputelement.options[inputelement.selectedIndex].value;
+    let population = 0 , literacyRate = 0, language = '  ';
+    
+switch(city){
+    case 'Bengaluru':
+        population = 8443675
+        literacyRate = 88.71
+        language = 'Kannada'
+        break;
+        case 'Chennai':
+            population = 4646732
+            literacyRate = 90.20
+            language = 'Tamil'
+            break
+        case 'Mumbai':
+            population = 12442373
+            literacyRate = 89.73
+            language = 'Marathi'
+            break
+        case 'Delhi':
+            population = 16787941
+            literacyRate = 86.20
+            language = 'Hindi'
+            break
+}
+let text = `The Indian city of ${city} has a population of ${population}. Language spoken is ${language} and literacy rate is ${literacyRate}%.`
+document.getElementById('result').innerHTML = text;
+}
+
+
